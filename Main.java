3896057623458d08f65e1415ce6ca7c30f97ea55
ElenaTyutina <85786291+ElenaTyutina@users.noreply.github.com>
2021-07-25 23:53:32 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 550;
-        int add = 1000;
+        int add = 1001;
         int bonusCost = 100;
         int cashback = 1;
         int bonusBalance;
-        if (add >= 1000)
-        {
+        if (add > 1000) {
             bonusBalance = add * cashback / bonusCost;
-            System.out.println(bonusBalance);
+        } else {
+            bonusBalance = 0;
         }
-        else {
-            bonusBalance = 0 ;
-        }
-        int total = bonusBalance + balance + add ;
+        int total = bonusBalance + balance + add;
+        System.out.println(bonusBalance);
         System.out.println(total);
     }
 }
